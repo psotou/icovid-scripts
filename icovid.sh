4bb03ea6ca9d19df19ad9b6c84e38e64558a44b0
psotou <pascualsu@gmail.com>
2021-02-01 18:47:41 -0300

    mejoras en bash script

--- icovid.sh
+++ icovid.sh
@@ -7,3 +7,13 @@ mkdir /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE};
 
 # python3 step_one.py ${EXECUTIONDATE}
 python3 step_two.py ${EXECUTIONDATE}
+
+# para el back-up
+EXECUTIONDATEBACKUP=`date +%d%m%Y`;
+
+mkdir /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
+
+cp -r /home/pas/data-gob/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
+cp -r /home/pas/data-gob/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
+cp -r /home/pas/data-gob/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
+cp -r /home/pas/data-gob/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
