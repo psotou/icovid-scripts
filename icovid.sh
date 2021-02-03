9a9b90f76f01663ed298bd7705a6a5846c84f7cd
psotou <pascualsu@gmail.com>
2021-02-03 09:36:14 -0300

    se agrega generación de carpetas backup para los archivos generados y los que están el repo

--- icovid.sh
+++ icovid.sh
@@ -11,9 +11,14 @@ python3 step_two.py ${EXECUTIONDATE}
 # para el back-up
 EXECUTIONDATEBACKUP=`date +%d%m%Y`;
 
-mkdir /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
+mkdir /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
 
-cp -r /home/pas/data-gob/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
-cp -r /home/pas/data-gob/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
-cp -r /home/pas/data-gob/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
-cp -r /home/pas/data-gob/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/${EXECUTIONDATEBACKUP};
+cp /home/pas/python/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+cp /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+
+mkdir /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+
+cp -r /home/pas/datagovuc/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+cp -r /home/pas/datagovuc/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+cp -r /home/pas/datagovuc/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+cp -r /home/pas/datagovuc/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
