fce00ffac4ab75972cdb64830e607a7ad457b212
psotou <pascualsu@gmail.com>
2021-02-17 16:10:39 -0300

    Se mejora el flujo de ejecución de bash script para cuando ya se ha ejecutado el proceso más de una vez el mismo día

--- icovid.sh
+++ icovid.sh
@@ -1,24 +1,57 @@
 #!/usr/bin/bash
 
-EXECUTIONDATE=`date +%Y%m%d%H%M`; # YYYYMMDDHHmm
+EXECUTIONDATE=`date +%Y%m%d%H%M`;   # YYYYMMDDHHmm
+EXECUTIONDATEBACKUP=`date +%d%m%Y`; # para los bakcups
+UNIXTIME=`date +%s`;                # genera unix time para diferencias las carpetas de backup cuando hay más de una generada por día
+
+echo "[`(date +"%F %T")`] Generamos las carpetas con la fecha en formato YYYYMMDDHHmm de acuerdo al momento de ejecución"
 
 mkdir /home/pas/python/icovid-scripts/archivos-step-one/${EXECUTIONDATE};
 mkdir /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE};
 
-# python3 step_one.py ${EXECUTIONDATE}
-python3 step_two.py ${EXECUTIONDATE}
+echo "[`(date +"%F %T")`] Ejecutamos el py script que generas los archivos para el sitio ICOVID"
 
-# para el back-up
-EXECUTIONDATEBACKUP=`date +%d%m%Y`;
+python3 step_two.py ${EXECUTIONDATE};
 
-mkdir /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+echo "[`(date +"%F %T")`] Copiamos los archivos generados para el sitio ICOVID en la ruta /backup/generated/"
 
-cp /home/pas/python/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
-cp /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+if [ ! -d "/home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP}" ]; then
+  mkdir /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  cp /home/pas/python/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  cp /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+else
+  mkdir /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP}_${UNIXTIME};
+  mv /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP}/* /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP}_${UNIXTIME};
+  cp /home/pas/python/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  cp /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+fi
 
-mkdir /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+echo "[`(date +"%F %T")`] Copiamos las carpetas de interés del repo actualizadas en la ruta /backup/repo/"
 
-cp -r /home/pas/datagovuc/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
-cp -r /home/pas/datagovuc/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
-cp -r /home/pas/datagovuc/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
-cp -r /home/pas/datagovuc/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+
+if [ ! -d "/home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP}" ]; then
+  mkdir /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+else
+  mkdir /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP}_${UNIXTIME};
+  mv /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP}/* /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP}_${UNIXTIME};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+  cp -r /home/pas/datagovuc/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+fi
+
+# mkdir /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+
+# cp /home/pas/python/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+# cp /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+
+# mkdir /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+
+# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
+# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
