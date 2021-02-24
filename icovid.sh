f52f352db0fd903fe28fde01c9943fe31b810460
psotou <pascualsu@gmail.com>
2021-02-23 23:40:16 -0300

    New update 2021-02-23 23:40:16

--- icovid.sh
+++ icovid.sh
@@ -43,15 +43,3 @@ else
   cp -r /home/pas/datagovuc/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
   cp -r /home/pas/datagovuc/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
 fi
-
-# mkdir /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
-
-# cp /home/pas/python/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
-# cp /home/pas/python/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/pas/python/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
-
-# mkdir /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
-
-# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension1 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
-# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension2 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
-# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension3 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
-# cp -r /home/pas/datagovuc/icovid/ICOVID/dimension4 /home/pas/python/icovid-scripts/backup/repo/${EXECUTIONDATEBACKUP};
